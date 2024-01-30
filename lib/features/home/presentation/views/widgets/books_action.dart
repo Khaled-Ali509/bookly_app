9862425413c7f429aa50775ed4410e0b9a0b095c
Khaled-Ali509 <khalidalyhassan@gmail.com>
2024-01-30 08:03:11 +0200

    book details done

--- lib/features/home/presentation/views/widgets/books_action.dart
+++ lib/features/home/presentation/views/widgets/books_action.dart
@@ -1,13 +1,13 @@
 import 'package:bookly_app/features/home/data/models/Book_model.dart';
 import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher.dart';
-
+import '../../../../../core/utils/functions/launch_url.dart';
 import '../../../../../core/utils/styls.dart';
 import '../../../../../core/widgets/custom_button.dart';
 
 class BooksAction extends StatelessWidget {
   const BooksAction({Key? key, required this.bookModel}) : super(key: key);
- final BookModel bookModel;
+  final BookModel bookModel;
+
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -19,39 +19,51 @@ class BooksAction extends StatelessWidget {
         children: [
           Expanded(
             child: CustomButton(
-              onPressed: (){},
-              textStyle: Styles.textStyle14.copyWith(color: Colors.black,
+              onPressed: () {},
+              textStyle: Styles.textStyle14.copyWith(
+                color: Colors.black,
               ),
               backgroundColor: Colors.white,
               borderRadius: const BorderRadius.only(
-                bottomLeft:Radius.circular(16.0) ,
+                bottomLeft: Radius.circular(16.0),
                 bottomRight: Radius.zero,
-                topLeft:Radius.circular(16.0) ,
+                topLeft: Radius.circular(16.0),
                 topRight: Radius.zero,
-              ), text: 'Free',
+              ),
+              text: 'Free',
             ),
           ),
           Expanded(
             child: CustomButton(
-              onPressed: () async {
-                Uri uri = Uri.parse(bookModel.volumeInfo.previewLink!);
-                  if (await canLaunchUrl(uri) ) {
-                    launchUrl(uri);
-                  }
+              onPressed: () {
+                launchCustomUrl(
+                  context,
+                  bookModel.volumeInfo.previewLink,
+                );
               },
               backgroundColor: const Color(0xffEF8262),
-              textStyle:Styles.textStyle16.copyWith(color: Colors.white,
+              textStyle: Styles.textStyle16.copyWith(
+                color: Colors.white,
               ),
               borderRadius: const BorderRadius.only(
-                bottomLeft:Radius.zero,
-                bottomRight: Radius.circular(16.0) ,
-                topLeft:Radius.zero,
-                topRight:Radius.circular(16.0) ,
-              ), text: 'Free preview',
+                bottomLeft: Radius.zero,
+                bottomRight: Radius.circular(16.0),
+                topLeft: Radius.zero,
+                topRight: Radius.circular(16.0),
+              ),
+              text: getText(bookModel),
             ),
           ),
         ],
       ),
     );
+  }
+
+  getText(BookModel bookModel) {
+    if (bookModel.volumeInfo.previewLink == null) {
+      return 'Not Available';
+    } else {
+      return 'Free preview';
+    }
   }
 }
